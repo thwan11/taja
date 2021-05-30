1d71dcae53e76630da00fb3581a136a11473c715
thwan11 <dmkthwan11@gmail.com>
2021-05-31 05:15:00 +0900

    add TY

--- main.c
+++ main.c
@@ -9,16 +9,61 @@
 #include <Windows.h>
 #include <conio.h>
 
+//========================== 전처리기
+// 김태환
 #define WIDTH 128
 #define HEIGHT 32
 #define SIZE 6
 #define TIME_LIMIT 5
 
-void gotoxy(int, int);
-void show_string(char *);
-void ShowBox();
-void ShowHealth(int);
-void SetColor(unsigned short, unsigned short);
+// 김태영
+#define CHILD_CLEAR 150         /*유아기 클리어 점수*/
+#define ADOLESENT_CLEAR 300      /*청년기 클리어 점수*/
+#define OLD_CLEAR 450         /*노년기 클리어 점수*/
+#define LIMIT_TIME 15         /*단어하나당 주어진 시간*/
+
+// 조현영
+
+
+// 김재윤
+
+
+//========================== 함수 선언
+// 김태환
+void gotoxy(int, int);							// 커서 이동	
+void show_string(char *);						// 문자열 출력
+void ShowBox();									// 박스 출력
+void ShowHealth(int);							// 체력 출력
+void SetColor(unsigned short, unsigned short);	// 색 변경
+
+// 김태영
+void time_score();            //남은 시간에 따른 점수
+void show_score_CHILD();      //유아기 점수 계산하고 출력하는 함수
+void show_score_ADOLESENT();   //청년기 점수 계산하고 출력하는 함수
+void show_score_OLD();         //노년기 점수 계산하고 출력하는 함수
+int update_correct();         //맞은 개수 +1하고 저장하는 함수
+int update_difficult();         //틀린 개수 +1하고 저장하는 함수 <=== 틀린 개수가 아니라 어려운 거 맞은 개수??
+
+// 조현영
+
+
+// 김재윤
+
+
+//========================== 전역 변수
+// 김태환
+
+
+// 김태영
+int score = 0;               //점수 저장 변수, 0으로 초기화
+int correct = 0, difficult = 0;//correct: 맞은 기본단어 개수, difficult: 맞은 어려운 단어 개수
+int timescore;               //남는 시간에 따른 추가 점수
+int remain_time;            //타이핑 후 남은 시간
+
+// 조현영
+
+
+// 김재윤
 
 int main() {
 	char word[SIZE], ch, input[SIZE];
@@ -40,7 +85,7 @@ int main() {
 		if (time(0) == s_time + TIME_LIMIT)
 		{
 			system("cls");
-			health -= 2;
+			health -= 2;	// 시간 초과 체력 -2
 			ShowBox();
 			ShowHealth(health);
 			show_string(word);
@@ -53,12 +98,11 @@ int main() {
 			ch = _getch();
 			if (ch == 27)
 				break;
-			if (j > 0)
+			if (ch == 8)
 			{
-				if (ch == 8)
+				if (j > 0)
 				{
 					printf("\b \b");
-					input[j] = 0;
 					j--;
 				}
 			}
@@ -79,7 +123,7 @@ int main() {
 				Sleep(1000);
 			}
 			else
-				health--;
+				health--;	// 오타 체력 -1
 
 			system("cls");
 			ShowBox();
@@ -96,6 +140,9 @@ int main() {
 	return 0;
 }
 
+
+
+// 커서 이동
 void gotoxy(int x, int y)
 {
 	COORD Pos;
@@ -103,7 +150,7 @@ void gotoxy(int x, int y)
 	Pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
-
+// 문자열 출력
 void show_string(char* w)
 {
 	int i, x=15;
@@ -115,7 +162,7 @@ void show_string(char* w)
 	printf("%s\n", w);
 	gotoxy(127/2 - 5/2, 19);
 }
-
+// 박스 출력
 void ShowBox()
 {
 	int x = 9, y = 8, i;
@@ -169,25 +216,64 @@ void ShowBox()
 		printf("─");
 	printf("┘");
 }
-
+// 체력 출력
 void ShowHealth(int health)
 {
 	int i;
 	
 	gotoxy(WIDTH - 30, 5);
-	printf("                    ");
+	printf("                    ");	// 하트 삭제
 	gotoxy(WIDTH - 37, 5);
-	SetColor(15, 4);
+	SetColor(15, 4);	// 글자 하양, 배경 진한 빨강
 	printf(" HP ");
-	SetColor(15, 0);
+	SetColor(15, 0);	// 글자 하양, 배경 검정
 	printf(" : ");
-	SetColor(12,0);
+	SetColor(12,0);		// 글자 빨강, 배경 검정
 	for (i = 0; i < health; i++)
-		printf("\u2665");
-	SetColor(15, 0);
+		printf("\u2665");	// 하트 출력
+	SetColor(15, 0);	// 글자 하양, 배경 검정
 }
-
+// 색 변경
 void SetColor(unsigned short text, unsigned short back)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text | (back << 4));
+}
+
+
+// 김태영
+// 남은 시간에 따른 점수
+void time_score()
+{
+	timescore = 150 * (remain_time / LIMIT_TIME);
+	score += timescore;
+}
+// 유아기 점수 출력
+void show_score_CHILD()
+{
+	score += 50 * update_correct() + 80 * update_difficult() + CHILD_CLEAR;
+	printf("점수는 %d 점 입니다.", score);
+}
+// 청년기 점수 출력
+void show_score_ADOLESENT()
+{
+	score += 50 * update_correct() + 80 * update_difficult() + ADOLESENT_CLEAR;
+	printf("점수는 %d 점 입니다.", score);
+}
+// 노년기 점수 출력
+void show_score_OLD()
+{
+	score += 50 * update_correct() + 80 * update_difficult() + OLD_CLEAR;
+	printf("점수는 %d 점 입니다.", score);
+}
+// 맞은 개수 +1
+int update_correct()
+{
+	correct++;         //기본 단어 맞은 개수 +1
+	return correct;
+}
+// 틀린 개수 +1
+int update_difficult()
+{
+	difficult++;      //어려운단어 맞은 개수 +1
+	return difficult;
 }
